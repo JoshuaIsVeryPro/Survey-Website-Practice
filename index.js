144fc17be4611124682af2ba19858a31f10a527a
JoshuaIsVeryPro <joshuaterunez1@gmail.com>
2025-09-13 08:48:33 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let research = prompt('What is your favorite food to cook?', '');
+
+if(research){
+alert(`${research}`);
+}
+else if (research === ''|| research ==='null'){
+  alert('Cancelled');
+}
+
+confirm(`${research} is your favorite food to make?`);
+
+if (true){
+  alert(src=`"google.com/search?q="${research}`);
+}
