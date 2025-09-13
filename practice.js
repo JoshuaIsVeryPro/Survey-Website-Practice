81decc1f1ad67554e0418840ee5ee48759f8723f
JoshuaIsVeryPro <joshuaterunez1@gmail.com>
2025-09-13 08:35:41 +0800

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,3 +1,14 @@
-for (;;){
-  
+let research = prompt('What is your favorite food to cook?', '');
+
+if(research){
+alert(`${research}`);
+}
+else if (research === ''|| research ==='null'){
+  alert('Cancelled');
+}
+
+confirm(`${research} is your favorite food to make?`);
+
+if (true){
+  alert(src=`"google.com/search?q="${research}`);
 }
